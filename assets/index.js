43e422a2fa5b9fd1e8dfdfaa55389eda5bfd78b7
Parra <andresparraarze@gmail.com>
2022-05-20 11:40:02 -0400

    update

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,13 @@
 var password=document.getElementById("password");
 
-var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var carac = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var passwordLength = 15
 var password = "";
 var choice = passwordLength;
 
 for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber +1);
+    var randomNumber = Math.floor(Math.random() * carac.length);
+    password += carac.substring(randomNumber, randomNumber +1);
 }
 
 document.getElementById("password").value = password;
@@ -15,15 +15,19 @@ document.getElementById("password").value = password;
 var password=document.getElementById("password");
 
 function genPassword() {
- 
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var carac = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var passwordLength = 15
     var password = "";
-    var choice = passwordLength;
-    
+
+    alert("Select options");
+    window.confirm("Do you want capital letters?");
+    window.confirm("Do you want Numbers?");
+    window.confirm("Do you want Special characters?");
+
     for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber +1);
+    var randomNumber = Math.floor(Math.random() * carac.length);
+    password += carac.substring(randomNumber, randomNumber +1);
     } 
     alert ("your password is " + password);
 }
